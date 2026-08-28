bbe3a979754a187327794208544765b23598ab7b
martinmartiineez <martinmartinezcortizas@gmail.com>
2026-08-28 17:50:07 +0200

    Conectar web con Google Sheets

--- data-config.js
+++ data-config.js
@@ -1,4 +1,10 @@
 window.OFARO_CONFIG = {
   apiUrl: "",
-  sheetUrl: "https://docs.google.com/spreadsheets/d/1I852Llhr3Nj2LuR1TESXwYZ54hlPNQj30GU8GU5uSaI/edit"
+  spreadsheetId: "1I852Llhr3Nj2LuR1TESXwYZ54hlPNQj30GU8GU5uSaI",
+  sheetUrl: "https://docs.google.com/spreadsheets/d/1I852Llhr3Nj2LuR1TESXwYZ54hlPNQj30GU8GU5uSaI/edit",
+  sheets: {
+    carta: "Carta",
+    menu: "Menu del dia",
+    config: "Configuracion"
+  }
 };
