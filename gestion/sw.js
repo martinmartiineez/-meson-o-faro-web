6a4bcebabbea8d361316f4728751fa68f6cdc77e
martinmartiineez <martinmartinezcortizas@gmail.com>
2026-09-02 17:26:18 +0200

    fix: refrescar cache PWA promociones

--- gestion/sw.js
+++ gestion/sw.js
@@ -1,5 +1,5 @@
-const CACHE = 'ofaro-gestion-v4-20260902';
-const SHELL = ['./','index.html','styles.css?v=20260901-2','app.js?v=20260901-2','remote-print.js?v=20260902-1','promotions.js?v=20260902-1','manifest.webmanifest','icon.svg'];
+const CACHE = 'ofaro-gestion-v5-20260902';
+const SHELL = ['./','index.html','styles.css?v=20260901-2','app.js?v=20260901-2','remote-print.js?v=20260902-1','promotions-compat.js?v=20260902-2','promotions.js?v=20260902-2','manifest.webmanifest','icon.svg'];
 self.addEventListener('install', event => {
   event.waitUntil(caches.open(CACHE).then(cache => cache.addAll(SHELL)).then(() => self.skipWaiting()));
 });
