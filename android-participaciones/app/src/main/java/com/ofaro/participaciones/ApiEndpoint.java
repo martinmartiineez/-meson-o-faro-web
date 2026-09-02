8a81571a88f1cff69af2be3635951eeb890aaaae
martinmartiineez <martinmartinezcortizas@gmail.com>
2026-09-02 19:15:04 +0200

    fix: añadir endpoint API público nuevo para Android

---
+++ android-participaciones/app/src/main/java/com/ofaro/participaciones/ApiEndpoint.java
@@ -0,0 +1,6 @@
+package com.ofaro.participaciones;
+
+final class ApiEndpoint {
+    private ApiEndpoint() {}
+    static final String URL = "https://script.google.com/macros/s/AKfycbxPSc2Ep3IkZ7lDV1tkm3dnBWgMV0QVrRfx50WJJJXN-q1xELZSWrfdJt3lTxaAXG2miA/exec";
+}
